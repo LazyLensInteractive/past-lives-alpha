698e2e4d5b1e0ddf8d0164cb40862ba139dd5b82
LazyLensInteractive <89745002+LazyLensInteractive@users.noreply.github.com>
2026-06-03 21:08:13 +1000

    stuff
    work on after the fall tomorrow

---
+++ scenes/control_for_after_fall_game_start.gd
@@ -0,0 +1,15 @@
+extends Node
+#control for after the emit happens from the floor 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_falling_room_start_episode_0() -> void:
+	pass # Replace with function body.
