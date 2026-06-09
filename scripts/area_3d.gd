4a76fd35cb162b4c2934f278fbf41edfd8bc7cb0
LazyLensInteractive <89745002+LazyLensInteractive@users.noreply.github.com>
2026-06-09 23:57:31 +1000

    removed rapier physics caused crashes and working on more stuff as per usual
    cassetee menu system bare bones really need a better whay to do this

--- scripts/area_3d.gd
+++ scripts/area_3d.gd
@@ -1,25 +1,32 @@
 extends Area3D
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+
 @onready var stars: GPUParticles3D = $"../../GPUParticles3D"
 @onready var main: AnimationPlayer = $"../../Main"
-
+signal cassette_in
 func _on_body_entered(body: Node3D) -> void:
+	
 	print("enter")
 	var disc_parrent = body.get_parent()
 	if "disc_id" in disc_parrent:
+		monitoring = false
+		cassette_in.emit()
 		print(disc_parrent.disc_id)
 		datamanger.current_disc = disc_parrent.disc_id
-		print(datamanger.current_disc)
-		var disc = datamanger.current_disc
-		menu_upadte(disc)
+		#print(datamanger.current_disc)
+		#var disc = datamanger.current_disc
+		#menu_upadte(disc)
 	else:
 		print(body)
 
 
 func menu_upadte(disc: int):
 	print(disc, " menu update")
+	
+
+func _on_cassete_control_animation_finished(_anim_name: StringName) -> void:
+	var disc = datamanger.current_disc
 	if disc == 1:
 		var tween = create_tween()
 		tween.tween_property(stars, "amount_ratio", 0, 30)
 		main.play("play_game")
-		mesh_instance_3d.visible = false
+		
--- scripts/disc.gd
+++ scripts/disc.gd
@@ -1,11 +1,23 @@
 extends Node3D
 @export var disc_id = 0
+@onready var cassete_control: AnimationPlayer = $"../Table (after fall menu)/portable_cassette_player_1k/StaticBody3D/cassete body/cassete lid/cassete control"
+@onready var pickable_object: XRToolsPickable = $PickableObject
+@onready var marker_3d: Marker3D = $"../Table (after fall menu)/portable_cassette_player_1k/Marker3D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(pickable_object.global_position)
+
+
+func _on_area_3d_cassette_in() -> void:
+	print("yeah")
+	pickable_object.freeze = true
+	pickable_object.drop()
+	#var tween = create_tween()
+	#tween.tween_property(self, "global_position", marker_3d.global_position, 1.3)
+	cassete_control.play("lock cartridge")
