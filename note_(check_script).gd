9442b8b72f5f6ec9478b79b83694eaea308e67af
LazyLensInteractive <89745002+LazyLensInteractive@users.noreply.github.com>
2026-09-06 23:11:16 +1000

    grass and other shit
    text file for demo grass kinda exists now idk just trying to make a bit of a teaser for a video im working on

---
+++ note_(check_script).gd
@@ -0,0 +1,4 @@
+@warning_ignore("empty_file")
+# okay so pretty much going to have a few things here for the teaser for the video im working on ideas out here so i dont forget
+#player is laying in a grass field and alice speaks (demo-talk.json) 
+#
